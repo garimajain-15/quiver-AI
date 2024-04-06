55f1c9d440cfc3ef93eb486ac4f69f28dc37af32
Garima Jain <garima@hiddentalent.tech>
2024-04-06 14:41:41 +0530

    TASK- Added basic grid functionalities

--- src/app/syncfusion-data/syncfusion-data.component.ts
+++ src/app/syncfusion-data/syncfusion-data.component.ts
@@ -1,20 +1,25 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {
   ColumnChooserService,
   ExcelExportService,
   FilterService,
-  GridAllModule,
-  PageService, PdfExportService,
-  SortService, ToolbarService
+  GridAllModule, GridComponent,
+  PageService, PageSettingsModel, PdfExportService, pdfHeaderQueryCellInfo, PdfHeaderQueryCellInfoEventArgs,
+  SortService, ToolbarItems, ToolbarService
 } from "@syncfusion/ej2-angular-grids";
 import {SyncfusionDataService} from "./syncfusion-data.service";
 import {Subscription} from "rxjs";
+import {TooltipAllModule} from "@syncfusion/ej2-angular-popups";
+import {ButtonAllModule} from "@syncfusion/ej2-angular-buttons";
+import { ClickEventArgs } from '@syncfusion/ej2-navigations';
 
 @Component({
   selector: 'app-syncfusion-data',
   standalone: true,
   imports: [
-    GridAllModule
+    GridAllModule,
+    TooltipAllModule,
+    ButtonAllModule
   ],
   templateUrl: './syncfusion-data.component.html',
   styleUrl: './syncfusion-data.component.scss',
@@ -23,14 +28,22 @@ import {Subscription} from "rxjs";
 export class SyncfusionDataComponent implements OnInit, OnDestroy {
   public shareData$: Subscription = new Subscription();
   public data: [] = [];
+  public initialPageSettings: PageSettingsModel = {};
+  public gridToolbarOptions: ToolbarItems[] = ['ExcelExport', 'CsvExport', 'PdfExport', 'Print', 'ColumnChooser'];
+  @ViewChild('gridReference')
+  public gridReference?: GridComponent;
 
   constructor(private syncfusionDataService: SyncfusionDataService) {
   }
 
   ngOnInit() {
+    this.initialPageSettings = { pageSizes: true, pageCount: 4 };
     this.shareData$ = this.syncfusionDataService.shareData$.subscribe(data => {
       if (data) {
         this.data = data;
+        if (this.gridReference) {
+          this.gridReference.emptyRecordTemplate = 'No Records To Display.'
+        }
       }
     });
     this.syncfusionDataService.fetchData();
@@ -39,6 +52,25 @@ export class SyncfusionDataComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     if (this.shareData$) {
       this.shareData$.unsubscribe();
+    }
+  }
+
+  openAddDetailPage() {
+
+  }
+
+  onToolbarClick(event: ClickEventArgs) {
+    if (this.gridReference) {
+      this.syncfusionDataService.gridToolbarClickEvents(event, this.gridReference, 'Details', 480);
+    }
+  }
+
+  pdfHeaderQueryCellInfo(args: PdfHeaderQueryCellInfoEventArgs) {
+    if (args?.cell) {
+      const pdfCell: any = args.cell;
+      if (pdfCell?.row?.pdfGrid) {
+        pdfCell['row']['pdfGrid']['repeatHeader'] = true;
+      }
     }
   }
 }
--- src/app/syncfusion-data/syncfusion-data.service.ts
+++ src/app/syncfusion-data/syncfusion-data.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable, Subject} from 'rxjs';
+import {Subject} from 'rxjs';
+import {ExcelExportProperties, PdfExportProperties} from "@syncfusion/ej2-grids";
+import {GridComponent} from "@syncfusion/ej2-angular-grids";
+import { ClickEventArgs } from '@syncfusion/ej2-navigations';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +17,67 @@ export class SyncfusionDataService {
     return this.http.get<any>('./assets/JSON/sample-data.json').subscribe({next: data => {
       this.shareData.next(data);
     }});
+  }
+
+  gridToolbarClickEvents(args: ClickEventArgs, gridReference: GridComponent, componentName: string, pdfHeadingLeftPosition: number){
+    if (args?.item?.text && gridReference ) {
+      switch (args['item']['text']) {
+        case 'PDF Export':
+          this.pdfExportProperties(gridReference, componentName, pdfHeadingLeftPosition);
+          break;
+        case 'CSV Export':
+          const csvExportProperties: ExcelExportProperties = {
+            fileName: componentName + '.csv',
+            header: {
+              headerRows: 2,
+              rows: [
+                {
+                  cells: [{
+                    colSpan: gridReference?.getVisibleColumns()?.length, value: componentName
+                  }]
+                }]
+            },
+          };
+          gridReference?.csvExport(csvExportProperties);
+          break;
+        case 'Excel Export':
+          const excelExportProperties: ExcelExportProperties = {
+            header: {
+              headerRows: 1,
+              rows: [
+                {
+                  cells: [{
+                    colSpan: gridReference?.getVisibleColumns()?.length, value: componentName,
+                    style: {fontSize: 16, hAlign: 'Center', bold: true}
+                  }]
+                }]
+            },
+            fileName: componentName + '.xlsx'
+          };
+          gridReference?.excelExport(excelExportProperties);
+          break;
+      }
+    }
+
+  }
+
+  pdfExportProperties(gridReference: GridComponent, componentName: string, positionLeft: number){
+    let pdfExportProperties: PdfExportProperties  = {
+      fileName: componentName + ".pdf",
+      pageOrientation: 'Landscape',
+      header: {
+        fromTop: 0,
+        height: 50,
+        contents: [
+          {
+            type: 'Text',
+            value: componentName,
+            position: {x: positionLeft, y: 0},
+            style: {fontSize: 20},
+          }
+        ]
+      }
+    };
+    gridReference.pdfExport(pdfExportProperties);
   }
 }
